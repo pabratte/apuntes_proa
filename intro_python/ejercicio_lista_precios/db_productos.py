5daa4ddc812902ef82a69b2a7b7cc7f1b6eee843
Pablo Abratte <pabratte@gmail.com>
2023-08-08 18:15:59 -0300

    update

--- intro_python/ejercicio_lista_precios/db_productos.py
+++ intro_python/ejercicio_lista_precios/db_productos.py
@@ -1,38 +1,36 @@
+# -*- coding: utf-8 -*-
+import csv
+'''
+La función cargar_productos() carga los datos de los productos
+desde el archivo "productos.csv"
+* parámetros: no recibe
+* valor de retorno: devuelve una lista con la información de los productos,
+donde cada producto es un diccionario que contiene los siguientes campos:
+[
+	{
+		id: 1,
+		nombre: 'Disco Solido SSD - 240GB - GIGABYTE',
+		precio: 3696,
+	},
+    {
+		id: 2,
+		nombre: 'Mouse GAMER LOGITECH G300S GAMING',
+		precio: 2904,
+	},
+]
 
-import turtle
-colores=(str(input("ingrese color ")))
-def dibujar_cuad():
-        turtle.forward(30)
-        turtle.left(90)
-        turtle.forward(30)
-        turtle.left(90)
-        turtle.forward(30)
-        turtle.left(90)
-        turtle.forward(30)
-        turtle.left(90)
-
-def dibujar_roseton():
-        for i in range(20):
-
-            turtle.left(36)
-            turtle.color(colores)
-            dibujar_cuad()
-
-
-        for i in range(20):
-            turtle.left(28)
-            turtle.color('pink')
-            dibujar_cuad()
-def dibujar_palo():
-        turtle.color('green')
-        turtle.right(90)
-        turtle.forward(100)
-        turtle.forward(100)
-
-def dibujar_rosetonn():
-        dibujar_roseton()
-        dibujar_palo ()
-
-
-for i in range(11):
-    dibujar_rosetonn ()
+Ejemplo de uso:
+l = []
+l = cargar_productos()
+'''
+def cargar_productos():
+    productos = []
+    with open('productos.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            np = {}
+            np['id'] = int(row[0])
+            np['nombre'] = row[1].strip()
+            np['precio'] = float(row[2])
+            productos.append(np)
+    return productos
